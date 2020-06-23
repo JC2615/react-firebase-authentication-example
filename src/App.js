d931b7c1585af5c7484af8ebe56524fa4f15cd8f
Josh Curry <joshuacurry2615@gmail.com>
2020-06-23 19:52:18 -0400

    Inital commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import CreateUser from "./components/CreateUser";
+import Home from "./components/Home";
+
+// This site has 3 pages, all of which are rendered
+// dynamically in the browser (not server rendered).
+//
+// Although the page does not ever refresh, notice how
+// React Router keeps the URL up to date as you navigate
+// through the site. This preserves the browser history,
+// making sure things like the back button and bookmarks
+// work properly.
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/CreateUser">Create User</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/CreateUser">
+            <CreateUser />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+export default App;
---
+++ src/components/CreateUser.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDLkBnqgBnwfNBDilry1o8lH2EyRz7UWug",
+  authDomain: "react-example-26243.firebaseapp.com",
+  databaseURL: "https://react-example-26243.firebaseio.com",
+  projectId: "react-example-26243",
+  storageBucket: "react-example-26243.appspot.com",
+  messagingSenderId: "552439870652",
+  appId: "1:552439870652:web:75130398ac3460805f4cb9",
+  measurementId: "G-4JLD26QVTV"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+function test(){
+  let emailInput = document.getElementById('email');
+  var actionCodeSettings = {
+    // URL you want to redirect back to. The domain (www.example.com) for this
+    // URL must be whitelisted in the Firebase Console.
+    url: 'https://react-example-26243.firebaseapp.com/finishSignUp?cartId=1234',
+    // This must be true.
+    handleCodeInApp: true,
+  };
+  
+  firebase.auth().sendSignInLinkToEmail(emailInput.value, actionCodeSettings)
+  .then(function() {
+    // The link was successfully sent. Inform the user.
+    // Save the email locally so you don't need to ask the user for it again
+    // if they open the link on the same device.
+    window.localStorage.setItem('emailForSignIn', emailInput.value);
+    console.log("Email sent!")
+  })
+  .catch(function(error) {
+    // Some error occurred, you can inspect the code: error.code
+    console.log(error);
+  });
+
+}
+
+export default function CreateUser() {
+    return (
+      <div>
+        <h2>Create User</h2>
+        <input id="email" type="email"/> 
+        <div></div>
+        <button onClick={test}>Click me</button>
+      </div>
+    );
+  }
